ebde329bc0b99551ecfb830accc991830b23a364
Espoir <espoirchakpali15@gmail.com>
2025-08-26 11:22:59 +0000

    4th commit NAEMA_RENDER

--- start.sh
+++ start.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
-pip install -r requirements.txt
-streamlit run app.py --server.port $PORT --server.enableCORS false --server.headless true
+# Script pour lancer Streamlit sur Render de manière stable
 
+# Installer les dépendances
+pip install -r requirements.txt
+
+# Lancer Streamlit
+streamlit run app.py \
+    --server.port $PORT \
+    --server.headless true \
+    --server.enableCORS false \
+    --server.enableXsrfProtection false
